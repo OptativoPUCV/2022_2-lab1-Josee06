f7d693f6ea0bc8555350f045559c2978d5033986
Replit user <>
2022-08-15 02:32:25 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -66,7 +66,10 @@ typedef struct
 Persona* crearPersona(char nombre[], char rut[], int edad) 
 {
   Persona *vector = (Persona*) malloc(sizeof(Persona));
-  if(vector == NULL) exit(EXIT_FAILURE);
+  if(vector == NULL) 
+  {
+    exit(EXIT_FAILURE);
+  }
   else
   {
     strcpy(vector->nombre, nombre);
